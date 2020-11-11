a586e97a82c49e65a9348de9ccc03f87db9e481c
Andre <als260602@msn.com>
2020-11-11 17:05:49 -0300

    Criando clone da amazon praticando react-redux

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBZGm23U-bHHfmQ5xdHsmen2jfFvtrvO7w",
+  authDomain: "chalenge-99d37.firebaseapp.com",
+  databaseURL: "https://chalenge-99d37.firebaseio.com",
+  projectId: "chalenge-99d37",
+  storageBucket: "chalenge-99d37.appspot.com",
+  messagingSenderId: "276941837083",
+  appId: "1:276941837083:web:0dfa93f612b1846a56f9be"
+};
